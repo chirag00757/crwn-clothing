6d0617a931eb34d307cec23a431537325341b25a
Chirag <chirag00757@yahoo.in>
2020-06-13 18:21:46 -0500

    Added Header and styling to link

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch,Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 
@@ -10,13 +11,11 @@ function App() {
   return (
 
     <div >
+    <Header />
      <Switch>
       <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop' component={ShopPage} />
-    
-     </Switch>
-    
-    
+      <Route exact path='/shop' component={ShopPage} />    
+     </Switch>    
     </div>
   );
 }
