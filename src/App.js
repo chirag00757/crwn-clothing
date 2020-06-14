513d62c1de8b019f7a9171bcd1aa747058246acd
Chirag <chirag00757@yahoo.in>
2020-06-13 19:51:58 -0500

    Added sign In pages and formInput and customButtom components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component";
 
 
 
@@ -15,6 +16,7 @@ function App() {
      <Switch>
       <Route exact path='/' component={HomePage} />
       <Route exact path='/shop' component={ShopPage} />    
+      <Route exact path="/signIn" component={SignInAndSignUpPage} />
      </Switch>    
     </div>
   );
---
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.components';
+import './sign-in-sign-up.styles.scss';
+
+const SignInAndSignUpPage = () => (
+    <div className="sign-in-sign-up">
+       <div className="signIn">
+            <SignIn />
+       </div>
+    </div>
+)
+
+export default SignInAndSignUpPage;
