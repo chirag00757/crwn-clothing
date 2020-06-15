b3240759731b8c4e9c75092d190077a6951883be
Chirag <chirag00757@yahoo.in>
2020-06-15 16:19:46 -0500

    implemented firebase utils , include ability to stroe authenticated user

--- src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/sign-in.components';
+import SignUp from '../../components/sign-up/sign-up.components';
 import './sign-in-sign-up.styles.scss';
 
 const SignInAndSignUpPage = () => (
     <div className="sign-in-sign-up">
        <div className="signIn">
             <SignIn />
+       </div>
+       <div className="signUp">
+            <SignUp />
        </div>
     </div>
 )
