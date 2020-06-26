fe6c19e5f1b286e0007840f74578b288d7f64c41
Chirag <chirag00757@yahoo.in>
2020-06-26 16:19:52 -0500

    updating redirect and user action types define in object for igniring typo error

--- src/App.js
+++ src/App.js
@@ -47,18 +47,27 @@ class App extends Component{
        <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />    
-        <Route exact path="/signIn" component={SignInAndSignUpPage} />
+        <Route 
+               exact
+               path="/signin"
+               render={() => 
+                this.props.currentUser ? (
+                  <Redirect to='/' />
+
+                ) : (
+                  <SignInAndSignUpPage />
+                )}  />
        </Switch>    
       </div>
     );
   }
  
 }
-const mapStateToProps = user => ({
-  
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
 })
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
