dc6fb9fe921a6908edbb53fca37e922717a0bd99
Chirag Patel <chirag.patel@Chirags-MacBook-Pro.local>
2026-01-21 15:32:31 -0600

    Fix cloudflare deployment

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import {Provider} from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import {store,persistor} from './redux/store';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 
-ReactDOM.render(
-  <Provider store={store}>  
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-      
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </React.StrictMode>
     </BrowserRouter>
-  </Provider>
-  ,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root"),
 );
